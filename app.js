0b1b754628ec3c2d96581777640a68f39e7e0ef4
manisha badal <manishabadal@12gmail.com>
2024-06-18 14:48:24 +0545

    change on file

--- app.js
+++ app.js
@@ -1,92 +1,80 @@
 
 
-let gameSeq=[];
-let userSeq=[];
-let btns=['yellow','red','purple','green'];
+let gameSeq = [];
+let userSeq = [];
+let btns = ['red', 'yellow', 'green', 'blue'];
 
-let started=false;
-let level=0;
-let h2=document.querySelector('h2');
+let started = false;
+let level = 0;
+let h2 = document.querySelector('h2');
 
-document.addEventListener('keypress',function(){
-    if(started==false){
+document.addEventListener('keypress', function () {
+    if (started == false) {
         console.log('game is started');
-        started=true;
-      levelUp();
+        started = true;
+        levelUp();
     }
 });
-// when game click btn background color white
 
-function gameFlash(btn){
+function gameFlash(btn) {
     btn.classList.add('flash');
-    setTimeout(function(){
+    setTimeout(function () {
         btn.classList.remove('flash');
 
-    },250)
+    }, 250)
 }
 
-// when user click button background change green color
-function userFlash(btn){
+function userFlash(btn) {
     btn.classList.add('userflash');
-    setTimeout(function(){
+    setTimeout(function () {
         btn.classList.remove('userflash');
-
-    },250)
+    }, 250)
 }
 
-function levelUp(){
-    // level up vaisakey paxi userseq khali hunxa and user ley start deykhi button click garnu parxa
-    userSeq=[];
-
+function levelUp() {
+    userSeq = [];
     level++;
-    h2.innerText=`Level ${level}`;
-
-    let randomIdx=Math.floor(Math.random()*3);
-    let randomCol=btns[randomIdx];
-
-    let randomBtn=document.querySelector(`.${randomCol}`);
-//     console.log(randomIdx);+
-
-//    console.log(randomCol);
-//     console.log(randomBtn);
+    h2.innerText = `Level ${level}`;
+    let randomIdx = Math.floor(Math.random() * 3);
+    let randomCol = btns[randomIdx];
+    let randomBtn = document.querySelector(`.${randomCol}`);
     gameSeq.push(randomCol);
     console.log(gameSeq);
     gameFlash(randomBtn);
 }
-function checkAns(idx){
-  
-    if(userSeq[idx]==gameSeq[idx]){
-        if(userSeq.length==gameSeq.length){
-            setTimeout(levelUp,1000);
-        }
-    }else{
-        h2.innerHTML=`Game OVER! Your score was <b>${level}</b> <br>Press any key to start.`;
-        document.querySelector('body').style.backgroundColor="red";
-        setTimeout(function(){
-        document.querySelector('body').style.backgroundColor="white";
+function checkAns(idx) {
 
-        },150)
+    if (userSeq[idx] == gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game OVER! Your score was <b>${level}</b> <br>Press any key to start.`;
+        document.querySelector('body').style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector('body').style.backgroundColor = "white";
+
+        }, 150)
         reset();
     }
 }
-function btnPress(){
- let btn=this;
- userFlash(btn) ;
- userColor=btn.getAttribute('id');
- userSeq.push(userColor);
- checkAns(userSeq.length-1);
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+    userColor = btn.getAttribute('id');
+    userSeq.push(userColor);
+    checkAns(userSeq.length - 1);
 
 }
-let allBtns=document.querySelectorAll('.btn');
-for(btn of allBtns){
-    btn.addEventListener('click',btnPress);
+let allBtns = document.querySelectorAll('.btn');
+for (btn of allBtns) {
+    btn.addEventListener('click', btnPress);
 }
-// wrong btn press garda game freeze bhayeko hunaley paxi game lai reset garinxa
-function reset(){
-    started=false;
-    gameSeq=[];
-    userSeq=[];
-    level=0;
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
 
 
@@ -94,6 +82,6 @@ function reset(){
 
 
 
-    
-    
+
+
 
